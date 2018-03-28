bd091502f94cd647f399b3e637ad4952703a5e82
aLIEz2476 <sakuragun11@gmail.com>
2018-03-28 13:43:24 +0900

    1

---
+++ sharp_P94/sharp_P94/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_P94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            string b = a.ToString();
+            Console.WriteLine(b);
+
+            float c = 3.14f;
+            string d = c.ToString();
+            Console.WriteLine(d);
+
+            string e = "12345";
+            int f = Convert.ToInt32(e);
+            Console.WriteLine(f);
+
+            string g = "1.234";
+            float h = float.Parse(g);
+            Console.WriteLine(h);
+        }
+    }
+}
